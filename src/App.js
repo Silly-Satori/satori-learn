af66db56113198d6338c0d805ca250a2644c1e38
Manas Bhambhra <dellanbhambhra@gmail.com>
2024-03-11 10:55:41 +0530

    Revert "navbar"
    This reverts commit da92b3e0dbf1cad44ff296132cfdcf5636e7d173.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import { useContext, useEffect, useState } from "react";
-// import { RouterProvider } from "react-router-dom";
-// import { router } from "./routes/routes";
-// import { Navbar } from "./components/Navbar";
-// import { ThemeProvider } from "./context/ThemeContext";
-import Navbar from "./compo/Navbar";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/routes";
+import { Navbar } from "./components/Navbar";
+import { ThemeProvider } from "./context/ThemeContext";
 
-import "./index.css";
+import "./App.css";
 
 function App() {
   return (
-    <Navbar/>
+    <ThemeProvider>
+      <RouterProvider router={router}>
+      </RouterProvider>
+    </ThemeProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import App from './App';
 
-import ReactDOM from 'react-dom';
-
-ReactDOM.render(<App/>, document.getElementById("root"));
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <GoogleOAuthProvider clientId="833981128922-nmruj5618m4lclq033omj1s7nranbd3p.apps.googleusercontent.com">
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-//   </GoogleOAuthProvider>
-// );
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <GoogleOAuthProvider clientId="833981128922-nmruj5618m4lclq033omj1s7nranbd3p.apps.googleusercontent.com">
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </GoogleOAuthProvider>
+);
