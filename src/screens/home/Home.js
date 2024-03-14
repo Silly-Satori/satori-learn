d9840526e2dceb42e4f8d8aeb46464a45f3b5825
Shruti0505 <shrutia.mumbai@gmail.com>
2024-03-15 01:41:15 +0530

    added scroll view

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,9 +1,23 @@
 import "./styles/Home.css";
+import { useRef } from "react";
 import Button from "../../components/containers/Button";
 import Footer from "../../components/containers/Footer";
 import ToggleTheme from "../../components/containers/ToggleTheme";
 import Hovertext from "../../components/containers/Hovertext";
 function Home() {
+    const scroll1 = useRef(null);
+    const scroll2 = useRef(null);
+    const scroll3 = useRef(null);
+
+    function getTopPosition(element) {
+        let topPosition = 0;
+        while (element) {
+          topPosition += element.offsetTop;
+          element = element.offsetParent;
+        }
+        return topPosition;
+      }
+
   return (
     <div className="home">
       <div className="hero-div flex flex-col lg:flex-row items-center justify-between lg:px-[8%] lg:py-20 px-[5%] py-10">
@@ -26,29 +40,35 @@ function Home() {
       </div>
 
       {/** home page main comes here */}
-      <div className="relative">{/* will prolly put a scroll bar here */}
-      <hr className="border border-secondary  w-full absolute "/>
+    <div className="relative mt-4">{/* will prolly put a scroll bar here */}
+    <hr className="border border-secondary  w-full absolute "/>
 
-      <div className="homebtn flex justify-center relative z-20 items-center bottom-4 ">
-      <button className="homebt ">WHO WE ARE</button>
-      <button className="homebt ">WHAT ARE WE GIVING</button>
-      <button className="homebt">OUR GOALS</button>
+    <div className="homebtn flex justify-center relative z-20 items-center bottom-4 ">
+    <button className="homebt" onClick={()=>{
+        window.scrollTo({top:getTopPosition(scroll1.current) ,behavior: "smooth"})
+    }}>WHO WE ARE</button>
+    <button className="homebt" onClick={()=>{
+         window.scrollTo({top:getTopPosition(scroll2.current) ,behavior: "smooth"})
+    }}>WHAT ARE WE GIVING</button>
+    <button className="homebt" onClick={()=>{
+        window.scrollTo({top:getTopPosition(scroll3.current) ,behavior: "smooth"})
+    }}>OUR GOALS</button>
         </div>
 
-      </div>
-      <div className="main mx-28">
+    </div>
+      <div className="main mx-28 mt-4">
        
         {/* main content comes here*/}
-        <div className="block1 flex justify-center items-center">
+        <div className="block1 flex justify-center items-center my-4" id="section" ref={scroll1}>
           <span>We are a team of dedicated professionals committed to empowering individuals like you to reach your full potential. Our instructors are experts in their fields, bringing years of experience and knowledge to each course they teach. From industry professionals to renowned academics, our instructors are passionate about sharing their expertise with you.
           </span>
           <img src="/assets/home/image.png"/>
         </div>
-        <div className="block2 flex flex-row-reverse justify-center items-center">
+        <div className="block2 flex flex-row-reverse justify-center items-center" id="section" ref={scroll2}>
           <span>we offer a wide range of courses covering diverse topics, including business, technology, arts, language, health, and much more. Whether you're a beginner or an expert, there's something for everyone here. Our courses are designed to be engaging, interactive, and practical, ensuring that you not only learn but also apply what you learn in real-world situations.</span>
           <img src="/assets/home/image.png"/>
         </div>
-        <div className="block3 flex justify-center items-center">
+        <div className="block3 flex justify-center items-center" id="section" ref={scroll3}>
           <div>Our goal is to provide We pride ourselves on providing high-quality education at an affordable price. With our user-friendly platform, you can learn at your own pace, on your own schedule. Plus, our responsive customer support team is always here to assist you every step of the way.</div>
           <img src="/assets/home/image.png"/>
         </div>
