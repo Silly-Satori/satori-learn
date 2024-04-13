36236963fbc3e9166c79637bbba279745ec66406
Shruti Agrawal <Shrutia.mumbai@gmail.com>
2024-04-13 19:00:47 +0530

    color modified

--- src/screens/course/Course.js
+++ src/screens/course/Course.js
@@ -63,13 +63,13 @@ const Course = () => {
             </div>
             <div className="course-list  grid gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
                 {courses.map((course, index) => (
-                    <div key={index} className="course-card p-4 hover:border-secondary-contrast hover:border-bg-primary-contrast rounded-lg shadow-lg font-light text-center border w-64  mx-2 my-2  md:w-72 px-5 pb-5">
+                    <div key={index} className="course-card p-4 hover:border-primary rounded-lg shadow-lg font-light text-center border w-64  mx-2 my-2  md:w-72 px-5 pb-5">
                         {/* <img src={`/assets/courses/${course.authorImage}`} alt={course.name} /> */}
                         <img src={image} alt={course.name} />
                         <h2 className='font-bold'>{course.name}</h2>
                         <p>{course.description}</p>
-                        <p>Price: ${course.price}</p>
-                        <button className='rounded-lg bg-secondary-contrast border px-2 py-1 font-medium text-bg-contrast m-2'>                        
+                        <p className='text-secondary'>Price: ${course.price}</p>
+                        <button className='rounded-lg bg-primary border px-2 py-1 font-medium text-bg-contrast m-2'>                        
                         <p>{course.bought ?  'Bought' : 'Buy Now'}</p>
                         </button>
                         <div className="tags">
