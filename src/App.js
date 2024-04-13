2d01eea0ce4457a92bc63a65571834d027b984fa
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-04-14 02:03:16 +0530

    Fixed themes, dynamic course loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useContext, useEffect, useState } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/routes";
-import { Navbar } from "./components/Navbar";
 import { ThemeProvider } from "./context/ThemeContext";
+import ToggleTheme from "./components/containers/ToggleTheme";
 
 import "./App.css";
 
 function App() {
   return (
     <ThemeProvider>
+      <div className="hidden">
+        <ToggleTheme/>
+      </div>
       <RouterProvider router={router}>
       </RouterProvider>
     </ThemeProvider>
--- src/screens/course/Course.js
+++ src/screens/course/Course.js
@@ -1,61 +1,85 @@
-import React from 'react';
-import image from './images/undraw_react_re_g3ui.svg';
-import './styles/Course.css';
+import React, { useEffect, useState } from "react";
+import image from "./images/undraw_react_re_g3ui.svg";
+import axios from "axios";
+import "./styles/Course.css";
 const Course = () => {
     // Dummy list of courses
-    const courses = [
+
+    const [courses, setCourses] = useState([]);
+
+    useEffect(() => {
+        axios.get(`http://localhost:8000/courses/fetch/0`).then((res) => {
+
+            setCourses(res.data);
+            let copy = [...res.data];
+            let i = 0;
+            copy.forEach((course) => {
+                course.authorImage = "undraw_next_js_-8-g5m.svg";
+                course.tags = ["tag1", "tag2", "tag3"];
+                course.bought = (i++ % 2 === 0);
+            });
+        });
+    }, []);
+
+
+    const courses1 = [
         {
-            name: 'React Js Course',
+            name: "React Js Course",
             price: 19.99,
             bought: true,
-            authorImage: 'undraw_next_js_-8-g5m.svg',
-            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-            tags: ['tag1', 'tag2', 'tag3'],
+            authorImage: "undraw_next_js_-8-g5m.svg",
+            description:
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            tags: ["tag1", "tag2", "tag3"],
         },
         {
-            name: 'Course 2',
+            name: "Course 2",
             price: 29.99,
             bought: false,
-            authorImage: 'author2.jpg',
-            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-            tags: ['tag4', 'tag5', 'tag6'],
+            authorImage: "author2.jpg",
+            description:
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            tags: ["tag4", "tag5", "tag6"],
         },
         {
-            name: 'JavaScript Course',
+            name: "JavaScript Course",
             price: 29.99,
             bought: true,
-            authorImage: 'author2.jpg',
-            description: 'loremsit amet, consectetur adipiscing elit',
-            tags: ['tag4', 'tag5', 'tag6'],
+            authorImage: "author2.jpg",
+            description: "loremsit amet, consectetur adipiscing elit",
+            tags: ["tag4", "tag5", "tag6"],
         },
         {
-            name: 'React Js Course',
+            name: "React Js Course",
             price: 19.99,
             bought: false,
-            authorImage: 'undraw_next_js_-8-g5m.svg',
-            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-            tags: ['tag1', 'tag2', 'tag3'],
+            authorImage: "undraw_next_js_-8-g5m.svg",
+            description:
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            tags: ["tag1", "tag2", "tag3"],
         },
         {
-            name: 'React Js Course',
+            name: "React Js Course",
             price: 19.99,
             bought: true,
-            authorImage: 'undraw_next_js_-8-g5m.svg',
-            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-            tags: ['tag1', 'tag2', 'tag3'],
+            authorImage: "undraw_next_js_-8-g5m.svg",
+            description:
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            tags: ["tag1", "tag2", "tag3"],
         },
         {
-            name: 'React Js Course',
+            name: "React Js Course",
             price: 19.99,
             bought: false,
-            authorImage: 'undraw_next_js_-8-g5m.svg',
-            description: 'lorem ipsum dolor sit amet, consectetur adipiscing elit',
-            tags: ['tag1', 'tag2', 'tag3'],
+            authorImage: "undraw_next_js_-8-g5m.svg",
+            description:
+                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
+            tags: ["tag1", "tag2", "tag3"],
         },
         // Add more courses here
     ];
-    const style = { style: { backgroundColour: 'var(--bg-contrast)' } };
-    
+    const style = { style: { backgroundColour: "var(--bg-contrast)" } };
+
     return (
         <div className="course flex flex-col items-center justify-center">
             <div className="course-header text-3xl font-bold flex items-center justify-start p-5">
@@ -63,14 +87,17 @@ const Course = () => {
             </div>
             <div className="course-list  grid gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
                 {courses.map((course, index) => (
-                    <div key={index} className="course-card p-4 hover:border-primary rounded-lg shadow-lg font-light text-center border w-64  mx-2 my-2  md:w-72 px-5 pb-5">
+                    <div
+                        key={index}
+                        className="course-card p-4 hover:border-primary rounded-lg shadow-lg font-light text-center border w-64  mx-2 my-2  md:w-72 px-5 pb-5"
+                    >
                         {/* <img src={`/assets/courses/${course.authorImage}`} alt={course.name} /> */}
                         <img src={image} alt={course.name} />
-                        <h2 className='font-bold'>{course.name}</h2>
+                        <h2 className="font-bold">{course.name}</h2>
                         <p>{course.description}</p>
-                        <p className='text-secondary'>Price: ${course.price}</p>
-                        <button className='bought rounded-lg bg-primary border px-2 py-1 font-medium text-bg-contrast m-2'>                        
-                        <p>{course.bought ?  'Bought' : 'Buy Now'}</p>
+                        <p className="text-secondary">Price: ${course.price}</p>
+                        <button className="bought rounded-lg bg-primary border px-2 py-1 font-medium text-bg-contrast m-2">
+                            <p>{course.bought ? "Bought" : "Buy Now"}</p>
                         </button>
                         <div className="tags">
                             {course.tags.map((tag, index) => (
