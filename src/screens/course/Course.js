81dc2a11ef3403260fe1e1baf696799ba6dd55a1
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-04-15 18:51:15 +0530

    Some changes to course

--- src/screens/course/Course.js
+++ src/screens/course/Course.js
@@ -2,10 +2,14 @@ import React, { useEffect, useState } from "react";
 import image from "./images/undraw_react_re_g3ui.svg";
 import axios from "axios";
 import "./styles/Course.css";
-const Course = () => {
-    // Dummy list of courses
+import Pagination from './Pagination';
 
+//actual courses we will be using from the backend
+const Course = () => {
     const [courses, setCourses] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    //change the coursesPerPage to whatever you want, I set as 2 just for testing and due to less courses
+    const [coursesPerPage] = useState(2); 
 
     useEffect(() => {
         let purchased = [];
@@ -40,63 +44,13 @@ const Course = () => {
             });
     }, []);
 
-    const courses1 = [
-        {
-            name: "React Js Course",
-            price: 19.99,
-            bought: true,
-            authorImage: "undraw_next_js_-8-g5m.svg",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            tags: ["tag1", "tag2", "tag3"],
-        },
-        {
-            name: "Course 2",
-            price: 29.99,
-            bought: false,
-            authorImage: "author2.jpg",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            tags: ["tag4", "tag5", "tag6"],
-        },
-        {
-            name: "JavaScript Course",
-            price: 29.99,
-            bought: true,
-            authorImage: "author2.jpg",
-            description: "loremsit amet, consectetur adipiscing elit",
-            tags: ["tag4", "tag5", "tag6"],
-        },
-        {
-            name: "React Js Course",
-            price: 19.99,
-            bought: false,
-            authorImage: "undraw_next_js_-8-g5m.svg",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            tags: ["tag1", "tag2", "tag3"],
-        },
-        {
-            name: "React Js Course",
-            price: 19.99,
-            bought: true,
-            authorImage: "undraw_next_js_-8-g5m.svg",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            tags: ["tag1", "tag2", "tag3"],
-        },
-        {
-            name: "React Js Course",
-            price: 19.99,
-            bought: false,
-            authorImage: "undraw_next_js_-8-g5m.svg",
-            description:
-                "lorem ipsum dolor sit amet, consectetur adipiscing elit",
-            tags: ["tag1", "tag2", "tag3"],
-        },
-        // Add more courses here
-    ];
+    const indexOfLastCourse = currentPage * coursesPerPage;
+    const indexOfFirstCourse = indexOfLastCourse - coursesPerPage;
+    const currentCourses = courses.slice(indexOfFirstCourse, indexOfLastCourse);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
     const style = { style: { backgroundColour: "var(--bg-contrast)" } };
+
 
     return (
         <div className="course flex flex-col items-center justify-center">
@@ -104,7 +58,7 @@ const Course = () => {
                 Courses
             </div>
             <div className="course-list  grid gap-12 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
-                {courses.map((course, index) => (
+                {currentCourses.map((course, index) => (
                     <div
                         key={index}
                         className="course-card p-4 hover:border-primary rounded-lg shadow-lg font-light text-center border w-64  mx-2 my-2  md:w-72 px-5 pb-5"
@@ -125,8 +79,16 @@ const Course = () => {
                     </div>
                 ))}
             </div>
+            <Pagination
+                coursesPerPage={coursesPerPage}
+                totalCourses={courses.length}
+                paginate={paginate}
+            />
         </div>
     );
 };
 
 export default Course;
+
+
+
