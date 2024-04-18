3bb1af9a14dda80d33dcde0e7cc97fa0b524d890
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-04-18 22:25:51 +0530

    Error fixed

--- src/screens/course/Course.jsx
+++ src/screens/course/Course.jsx
@@ -50,9 +50,9 @@ const Course = () => {
               } else {
                 course.bought = false;
               }
-              // send the "bought courses" to end of the list
-              copy.sort((a, b) => (a.bought === b.bought ? 0 : a.bought ? 1 : -1));
             });
+            // send the "bought courses" to end of the list
+            copy.sort((a, b) => (a.bought === b.bought ? 0 : a.bought ? 1 : -1));
             setCourses(copy);
           })
           .catch((error) => {
