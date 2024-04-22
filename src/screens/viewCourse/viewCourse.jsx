ea3febdc1223ea67212502203c9310b6e45379da
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2024-04-22 23:42:56 +0530

    Cleanup

--- src/screens/viewCourse/viewCourse.jsx
+++ src/screens/viewCourse/viewCourse.jsx
@@ -82,17 +82,6 @@ const viewCourse = () => {
             })
     }, [])
 
-    // useEffect(() => {
-    //     if (actualCurrentCourse.videos) {
-    //         const videos = actualCurrentCourse.videos.map((video) => (
-    //             <div key={video.id} className="py-2">
-    //                 <h4 className="text-lg font-semibold">{video.title}</h4>
-    //                 <p className="text-gray-600">{video.desc}</p>
-    //             </div>
-    //         ))
-    //         setActualCurrentCourse({ videos: videos })
-    //     }
-    // }, [actualCurrentCourse.videos])
 
     return (
         <>
@@ -184,37 +173,5 @@ const viewCourse = () => {
         </>
     )
 }
-
-// const viewCourse = () => {
-//   return (
-//     <div className="container mx-auto ml-14 mr-14  py-8 px-4 lg:flex">
-//       <div className="lg:w-2/3 lg:pr-11">
-//         <iframe
-//           title="Course Video"
-//           className="w-full h-64 lg:h-full lg:w-full"
-//           src={`https://www.youtube.com/embed/${currentCourse.videoId}`}
-//           frameBorder="0"
-//           allowFullScreen
-//         ></iframe>
-//         <div className="mt-6">
-//           <h2 className="text-2xl font-semibold">{currentCourse.title}</h2>
-//           <p className="text-gray-600 mt-2">{currentCourse.description}</p>
-//         </div>
-//       </div>
-
-//       <div className="lg:w-1/3 mt-8 lg:mt-0">
-//         <h3 className="text-xl font-semibold mb-4">Other Courses</h3>
-//         <div className="divide-y divide-gray-200">
-//           {otherCourses.map(course => (
-//             <div key={course.id} className="py-2">
-//               <h4 className="text-lg font-semibold">{course.title}</h4>
-//               <p className="text-gray-600">{course.description}</p>
-//             </div>
-//           ))}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
 
 export default viewCourse
